572a59e58cf8d5fc5ee34b18a98a05594e51bf76
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-09-21 10:33:18 -0300

    add logic

--- algorithmos/main.go
+++ algorithmos/main.go
@@ -1,7 +1,18 @@
 package main 
 
+
+func soma(x int, y int)  (int, bool) {
+    if x > 20 {
+		return x + y, true
+
+	}
+		return x + y, false
+	
+}
+
 func main() {
-	println("Hello World!")
+
+	resultado, status := soma(10,20)
 }
 
 
