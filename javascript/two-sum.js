f49ad4e5725dc3cfac758e20988b3026fa9d0778
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-10-11 14:18:44 -0300

    feat: improving algoritmo

--- javascript/two-sum.js
+++ javascript/two-sum.js
@@ -1,3 +1,5 @@
+// 1. Two Sum
+// Easy
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -25,13 +27,27 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let map = new Map()
-    for(let i = 0; i < nums.length; i++){
-      const complement = target - nums[i]
-      if(map.has(complement)){
-        return [i, map.get(complement)]
+    // Criamos uma "caixa" vazia chamada "numToIndex" para armazenar informações.
+    const numToIndex = new Map();
+  
+    // Iniciamos um loop que vai passar por todos os números no array "nums".
+    for (let i = 0; i < nums.length; i++) {
+      // Para cada número do array, calculamos qual outro número é necessário
+      // para que a soma dos dois seja igual ao "target" que queremos.
+      const complement = target - nums[i];
+  
+      // Verificamos se já encontramos esse "complemento" em algum número anterior.
+      if (numToIndex.has(complement)) {
+        // Se encontrarmos, isso significa que encontramos dois números que somam
+        // ao "target". Então, retornamos um array com as posições desses números.
+        return [i, numToIndex.get(complement)];
       }
-      map.set(nums[i], i)
+  
+      // Se não encontramos o "complemento" ainda, armazenamos o número atual e sua posição
+      // na "caixa" chamada "numToIndex". Isso nos ajuda a procurar o "complemento" posteriormente.
+      numToIndex.set(nums[i], i);
     }
-};
+  };
+
+//RUNTIME 48MS
  
