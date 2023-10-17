473cf11a15284bf807649734ce4792ada8e78d4f
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-10-16 23:08:39 -0300

    add two sum in golang

---
+++ golang/two-sum.go
@@ -0,0 +1,77 @@
+// 1. Two Sum
+// Solved
+// Easy
+// Topics
+// Companies
+// Hint
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+// Constraints:
+
+// 2 <= nums.length <= 104
+// -109 <= nums[i] <= 109
+// -109 <= target <= 109
+
+func twoSum_2ForLoopsSolution(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := 0; j < len(nums); j++ {
+			if nums[i]+nums[j] == target && i != j {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
+
+//OTHERS SOLUTIONS
+
+func twoSum_TwoPassSolution(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		m[nums[i]] = i
+	}
+
+	for i := 0; i < len(nums); i++ {
+		complement := target - nums[i]
+
+		index, containsComplement := m[complement]
+		if containsComplement && index != i {
+			return []int{i, index}
+		}
+	}
+
+	return []int{}
+}
+
+func twoSum_OnePassSolution(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		complement := target - nums[i]
+		index, containsComplement := m[complement]
+
+		if containsComplement {
+			return []int{i, index}
+		}
+
+		m[nums[i]] = i
+	}
+	return []int{}
+}
